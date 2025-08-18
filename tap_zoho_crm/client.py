88592ed73cd155254cf4c1731824ce4238705553
atttiwari <atul.tiwari@qlik.com>
2025-08-18 12:20:53 +0530

    SAC-28202: Update in client.py file for refresh token

--- tap_zoho_crm/client.py
+++ tap_zoho_crm/client.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict, Mapping, Optional, Tuple
+from datetime import datetime, timedelta
 
 import backoff
 import requests
@@ -10,6 +11,8 @@ from tap_zoho_crm.exceptions import ERROR_CODE_EXCEPTION_MAPPING, ZohoCRMError, 
 
 LOGGER = get_logger()
 REQUEST_TIMEOUT = 300
+REFRESH_URL = "https://accounts.zoho.com/oauth/v2/token"
+DEFAULT_EXPIRY_TIME_IN_SECONDS = 3600
 
 def raise_for_error(response: requests.Response) -> None:
     """Raises the associated response exception. Takes in a response object,
@@ -31,7 +34,7 @@ def raise_for_error(response: requests.Response) -> None:
             ).get("message", "Unknown Error")
             message = f"HTTP-error-code: {response.status_code}, Error: {response_json.get('message', error_message)}"
         exc = ERROR_CODE_EXCEPTION_MAPPING.get(response.status_code, {}).get(
-            "raise_exception", Zoho_CRMError
+            "raise_exception", ZohoCRMError
         )
         raise exc(message, response) from None
 
@@ -49,23 +52,78 @@ class Client:
         self.config = config
         self._session = session()
         self.base_url = "https://www.zohoapis.com/crm/"
+        self._access_token = None
+        self._expires_at = None
+        self._scope = None
+        self._api_domain = None
+        self._token_type = None
+
         config_request_timeout = config.get("request_timeout")
         self.request_timeout = float(config_request_timeout) if config_request_timeout else REQUEST_TIMEOUT
 
     def __enter__(self):
-        self.check_api_credentials()
+        self._refresh_access_token()
         return self
 
     def __exit__(self, exception_type, exception_value, traceback):
         self._session.close()
 
-    def check_api_credentials(self) -> None:
-        pass
+    def _refresh_access_token(self) -> None:
+        """Refreshes the access token."""
+        LOGGER.info("Refreshing Access Token")
+        resp_json = self.make_request(
+            "POST",
+            endpoint=REFRESH_URL,
+            headers={
+                "User-Agent": self.config["user_agent"],
+                "Content-Type": "application/json"
+            },
+            params={
+                "refresh_token": self.config["refresh_token"],
+                "client_id": self.config["client_id"],
+                "client_secret": self.config["client_secret"],
+                "grant_type": "refresh_token"
+            },
+            body={},
+            is_auth_req=False
+        )
+        LOGGER.info(resp_json)
+        self._access_token = resp_json.get("access_token")
+        self._scope = resp_json.get("scope")
+        self._api_domain = resp_json.get("api_domain")
+        self._token_type = resp_json.get("token_type", "Bearer")
+        expires_in_seconds = resp_json.get("expires_in", DEFAULT_EXPIRY_TIME_IN_SECONDS)
+        self._expires_at = datetime.now() + timedelta(seconds=expires_in_seconds)
+        LOGGER.info("Got refreshed access token")
+
+    def get_access_token(self) -> str:
+        """Return access token if available or generate one."""
+        if self._access_token and self._expires_at > datetime.now():
+            return self._access_token
+
+        self._refresh_access_token()
+        return self._access_token
+
+    @property
+    def headers(self) -> Dict[str, str]:
+        """
+        Construct and return the HTTP headers required for requests to the Amazon Advertising API.
+        """
+        header = {
+            'User-Agent': self.config["user_agent"],
+            'Content-Type': 'application/json'
+        }
+        return header
 
     def authenticate(self, headers: Dict, params: Dict) -> Tuple[Dict, Dict]:
         """Authenticates the request with the token"""
-        headers["Authorization"] = self.config["access_token"]
-        return headers, params
+        result_headers = self.headers.copy()
+        result_headers["Authorization"] = f"{self._token_type} {self.get_access_token()}"
+        if not headers:
+            result_headers.pop("Content-Type", None)
+        else:
+            result_headers.update(headers)
+        return result_headers, params
 
     def make_request(
         self,
@@ -74,7 +132,8 @@ class Client:
         params: Optional[Dict[str, Any]] = None,
         headers: Optional[Dict[str, Any]] = None,
         body: Optional[Dict[str, Any]] = None,
-        path: Optional[str] = None
+        path: Optional[str] = None,
+        is_auth_req: bool = True
     ) -> Any:
         """
         Sends an HTTP request to the specified API endpoint.
@@ -83,7 +142,8 @@ class Client:
         headers = headers or {}
         body = body or {}
         endpoint = endpoint or f"{self.base_url}/{path}"
-        headers, params = self.authenticate(headers, params)
+        if is_auth_req:
+            headers, params = self.authenticate(headers, params)
         return self.__make_request(
             method, endpoint,
             headers=headers,

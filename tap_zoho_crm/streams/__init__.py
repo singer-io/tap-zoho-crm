b78f02a370495ee8189926c11185dc7169b16c76
atttiwari <atul.tiwari@qlik.com>
2025-08-19 21:03:13 +0530

    SAC-28202: Update for unittest default implementaion and schema update

--- tap_zoho_crm/streams/__init__.py
+++ tap_zoho_crm/streams/__init__.py
@@ -1,19 +1,15 @@
-from tap_zoho_crm.streams.appointments import Appointments
 from tap_zoho_crm.streams.currencies import Currencies
 from tap_zoho_crm.streams.organization import Organization
 from tap_zoho_crm.streams.profiles import Profiles
 from tap_zoho_crm.streams.roles import Roles
-from tap_zoho_crm.streams.services import Services
 from tap_zoho_crm.streams.territories import Territories
 from tap_zoho_crm.streams.users import Users
 
 STREAMS = {
-    "appointments": Appointments,
     "currencies": Currencies,
     "organization": Organization,
     "profiles": Profiles,
     "roles": Roles,
-    "services": Services,
     "territories": Territories,
     "users": Users,
 }
